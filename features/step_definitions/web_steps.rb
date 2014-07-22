f4feb43c3f64ea3e60e1535f29a20d1a2a8e1cf3
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-07-22 17:52:56 -0400

    Showing a top navigation with the login status

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,3 +1,7 @@
 Then(/^(?:|I )should see "(.*?)"$/) do |text|
   expect(page.text).to include text
 end
+
+Then(/^I should see "(.*?)" in the top bar$/) do |text|
+  expect(page.find('.top-bar').text).to include text
+end
