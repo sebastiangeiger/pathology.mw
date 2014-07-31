ec388cd8b274610dc608b5f13c81f03d2687fb0e
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-07-31 10:14:06 -0400

    Designing the patient info box on the patient page

--- app/helpers/patient_helper.rb
+++ app/helpers/patient_helper.rb
@@ -1,4 +1,12 @@
 module PatientHelper
+  def patient_info_row(attribute, patient, optional: false)
+    method_name = attribute.downcase.gsub(" ","_").to_sym
+    value = patient.send(method_name)
+    %Q{<div class="row">
+         <div class="small-4 columns">#{attribute}:</div>
+         <div class="small-8 columns">#{optional_attribute(value)}</div>
+       </div>}.html_safe
+  end
   def optional_attribute(attribute)
     if attribute.nil? or attribute.blank?
       "<span class='not-set'>not set</span>".html_safe
