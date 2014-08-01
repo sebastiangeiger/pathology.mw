bc9748efd1af2b9f1ade1cb58eebf139c8054e11
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-08-01 18:18:56 -0400

    Table spans entire width of patients#index

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -1,7 +1,8 @@
 module TableHelper
-  def table_for(table_renderer)
+  def table_for(table_renderer, html: {})
     render partial: 'shared/basic_table',
       locals: { headers: table_renderer.headers,
-                rows: table_renderer.rows }
+                rows: table_renderer.rows,
+                html_options: html }
   end
 end
