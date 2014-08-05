c55300af53a0f87109daaee6490f00b2db432774
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-08-05 10:31:46 -0400

    Getting the specimen links

--- legacy_crawler/legacy_crawler.rb
+++ legacy_crawler/legacy_crawler.rb
@@ -45,12 +45,14 @@ class DataRepository
   end
 
   def add(keys, new_data)
+    keys = Array(keys)
     element = @data
     while key = keys.shift do
       if keys.empty?
         if new_data.is_a? Array
           element[key] ||= []
           element[key] += new_data
+          element[key] = element[key].uniq
         elsif new_data.is_a? Hash
           element[key] = new_data
         else
@@ -111,18 +113,19 @@ class PatientFinder
     end
   end
 
-  def patient_links_from(page)
-    page.links_with(text: /^\s*PHTRS\s+\d+$/).
-      map(&:uri).map(&:to_s)
-  end
-
   def get_all_patients
-    patient_links = @data_repository.get(:patient_links)#.first(1)
+    patient_links = @data_repository.get(:patient_links)
     patients = patient_links.each_with_index do |patient_link,i|
       puts "Patient Detail #{i+1}/#{patient_links.size}"
       patient = get_patient_detail(patient_link)
       @data_repository.add([:patients, patient[:id]], patient)
     end
+  end
+
+  private
+  def patient_links_from(page)
+    page.links_with(text: /^\s*PHTRS\s+\d+$/).
+      map(&:uri).map(&:to_s)
   end
 
   def get_patient_detail(patient_link)
@@ -135,6 +138,8 @@ class PatientFinder
       value = row.search("td").first.text
       result[key] = value
     end
+    result[:specimen_links] = page.links_with(href: /\/patients\/edit_specimen\//).
+      map(&:uri).map(&:to_s)
     raise unless result[:id] == (result[:patient_number].match(/^\s*PHRS\s+(\d+)/)[1].to_i)
     edit_link = result[:patient_link].gsub(/\/patient\//, "/edit/")
     page = agent.get(edit_link)
@@ -144,13 +149,56 @@ class PatientFinder
     result
   end
 
-  def run
-    get_all_patients
+  def agent
+    @agent
+  end
+end
+
+class SpecimenFinder
+  def initialize(configuration, data_repository)
+    @configuration = configuration
+    @agent = SignedInMechanize.new(Mechanize.new, @configuration)
+    @data_repository = data_repository
+  end
+
+  def get_all_specimen_links
+    page = agent.get(@configuration[:base_url] + "/results")
+    i = 0
+    while true
+      @data_repository.add(:specimen_links, specimen_links_from(page))
+      if next_page_link = page.link_with(text: ">")
+        page = next_page_link.click
+        puts %Q{Now on "#{page.uri.to_s}"}
+      else
+        break
+      end
+    end
   end
 
   private
+  def specimen_links_from(page)
+    page.links_with(href: /\/patients\/edit_specimen\//).
+      map(&:uri).map(&:to_s)
+  end
+
   def agent
     @agent
+  end
+end
+
+class Experiments
+  class << self
+    def compare_specimen_links_from_results_and_patients(data_repository)
+      patients = data_repository.get(:patients)
+      specimen_links_from_results = data_repository.get(:specimen_links)
+      specimen_links_from_patients = patients.values.map{|p| p[:specimen_links]}.flatten
+      puts "Difference between the speciment links:"
+      pp difference(specimen_links_from_patients, specimen_links_from_results)
+    end
+    private
+    def difference(array_a, array_b)
+      (array_a - array_b) | (array_b - array_a)
+    end
   end
 end
 
@@ -158,5 +206,7 @@ end
 data_repository = DataRepository.new('data.yml')
 configuration = YAML.load_file('config.yml')
 # 1. PatientFinder.new(configuration, data_repository).get_all_patient_links
-# 2.  PatientFinder.new(configuration, data_repository).get_all_patients
+# 2. PatientFinder.new(configuration, data_repository).get_all_patients
+# 3. SpecimenFinder.new(configuration, data_repository).get_all_specimen_links
 # data_repository.save!
+# Experiments.compare_specimen_links_from_results_and_patients(data_repository)
