a6eb9131ef466ab0f3667b19706d9fc137333247
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-04-05 00:33:57 -0400

    Adds Physician#full_name

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -2,4 +2,7 @@ class Physician < ActiveRecord::Base
 
   has_many :specimens
 
+  def full_name
+    "#{title} #{first_name} #{last_name}".strip
+  end
 end
--- spec/models/physician_spec.rb
+++ spec/models/physician_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe Physician, :type => :model do
 
     it { is_expected.to match_array [specimen] }
   end
+
+  describe '#full_name' do
+    let(:physician) { FactoryGirl.build(:physician, build_params) }
+    subject { physician.full_name }
+
+    context 'with a first and last name' do
+      let(:build_params) do
+        { title: nil,
+          first_name: "John",
+          last_name: "Doe" }
+      end
+      it { is_expected.to eql "John Doe" }
+    end
+
+    context 'with a first and last name and a title' do
+      let(:build_params) do
+        {  title: "Dr.",
+           first_name: "John",
+           last_name: "Doe" }
+      end
+      it { is_expected.to eql "Dr. John Doe" }
+    end
+
+  end
 end
