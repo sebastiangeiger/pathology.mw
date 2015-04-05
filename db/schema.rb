e50b57523520f88886339cc7129debe044938446
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-04-04 23:10:10 -0400

    Adding a Physician model
      * Had to delete the user factory physician
      * I don't want to tie the physician model to the user model just yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006200705) do
+ActiveRecord::Schema.define(version: 20150405030534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20141006200705) do
     t.integer  "birthyear"
     t.date     "imported_on"
     t.string   "legacy_link"
+  end
+
+  create_table "physicians", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "specimens", force: true do |t|
