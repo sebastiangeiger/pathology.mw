ad8ad2999397826e217ed1029f89c6fa277318d5
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-05-03 16:40:13 -0400

    Correcting rubocop offense

--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -2,6 +2,7 @@ class ActionView::Helpers::FormBuilder
   def combo_box(method, values, options = {})
     text_field(method, options.merge(data: { chosen_values: values }))
   end
+
   def chosen_select(method, choices, options = {}, html_options = {})
     select(method, choices, options, html_options.merge(data: { infect_with_chosen: true }))
   end
